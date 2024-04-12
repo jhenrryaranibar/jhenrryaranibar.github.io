e6a10c94dd2b00d4d44fccaf467eb1c0eac2076b
Jhenrry Aranibar <141357443+jhenrryaranibar@users.noreply.github.com>
2024-04-12 18:34:09 -0500

    behance

--- main.js
+++ main.js
@@ -21,45 +21,14 @@ function redirectToBehance() {
     var profileID = "jhenrryaranibar"; // Reemplaza esto con tu nombre de usuario de Behance
 
     // Detecta si el usuario está usando un dispositivo móvil
-    if (/android/i.test(userAgent) || /iPad|iPhone|iPod/.test(userAgent)) {
-        // Redirige a la aplicación de Behance en Android o iOS
+    if (/android/i.test(userAgent)) {
+        // Redirige a la aplicación de Behance en Android
+        window.location.href = "intent://profile/" + profileID + "#Intent;package=com.behance.behance;scheme=https;end;";
+    } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        // Redirige a la aplicación de Behance en iOS
         window.location.href = "behance://profile/" + profileID;
     } else {
         // Redirige al perfil de Behance en el navegador web para otras plataformas
         window.location.href = "https://www.behance.net/" + profileID;
-    }
-}
-
-function redirectToTikTok() {
-    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-    var profileID = "jhenrryaranibar"; // Reemplaza esto con tu nombre de usuario de TikTok
-
-    // Detecta si el usuario está usando un dispositivo móvil
-    if (/android/i.test(userAgent)) {
-        // Redirige a la aplicación de TikTok en Android
-        window.location.href = "intent://user/" + profileID + "#Intent;package=com.zhiliaoapp.musically;scheme=https;end;";
-    } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-        // Redirige a la aplicación de TikTok en iOS
-        window.location.href = "tiktok://user?user_id=" + profileID;
-    } else {
-        // Redirige al perfil de TikTok en el navegador web para otras plataformas
-        window.location.href = "https://www.tiktok.com/@" + profileID;
-    }
-}
-
-function redirectToLinkedIn() {
-    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-    var profileID = "jhenrryaranibar"; // Reemplaza esto con tu identificación de LinkedIn
-
-    // Detecta si el usuario está usando un dispositivo móvil
-    if (/android/i.test(userAgent)) {
-        // Redirige a la aplicación de LinkedIn en Android
-        window.location.href = "intent://profile/view?id=" + profileID + "#Intent;package=com.linkedin.android;scheme=https;end;";
-    } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-        // Redirige a la aplicación de LinkedIn en iOS
-        window.location.href = "linkedin://profile/view?id=" + profileID;
-    } else {
-        // Redirige al perfil de LinkedIn en el navegador web para otras plataformas
-        window.location.href = "https://www.linkedin.com/profile/view?id=" + profileID;
     }
 }
